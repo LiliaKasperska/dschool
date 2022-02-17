ee9ba424c90917a9c997fd611a06dd69fffd963c
LiliaKasperska <73168790+LiliaKasperska@users.noreply.github.com>
2022-02-17 23:53:15 +0200

    Add files via upload

---
+++ adm_header.php
@@ -0,0 +1,17 @@
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco_navbar ftco-navbar-light" id="ftco-navbar">
+	    <div class="container d-flex align-items-center">
+	    	<a class="navbar-brand" href="index.php" >Новосільський заклад загальної<br> середньої освіти І-ІІІ ступенів</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Меню
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	        	<li class="nav-item"><a href="adm_index.php" class="nav-link pl-0">Головна інформація</a></li>
+	        	<li class="nav-item"><a href="adm_t_add.php" class="nav-link">Вчителі</a></li>
+	        	<li class="nav-item"><a href="adm_downl_file_add.php" class="nav-link">Документація</a></li>
+	        	<li class="nav-item"><a href="adm_news_add.php" class="nav-link">Новини</a></li>
+	          <li class="nav-item"><a href="adm_question.php" class="nav-link">Запитання </a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
